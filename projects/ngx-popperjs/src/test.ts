1e3f5f8f0da109dc05c7b528a3f0e6bf6c8de788
Tony Samperi <github@tonysamperi.it>
2022-12-13 22:41:35 +0100

    Improved testing

--- projects/ngx-popperjs/src/test.ts
+++ projects/ngx-popperjs/src/test.ts
@@ -2,18 +2,19 @@
 
 import "zone.js/dist/zone";
 import "zone.js/dist/zone-testing";
-import { getTestBed } from "@angular/core/testing";
+import {getTestBed} from "@angular/core/testing";
 import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting
 } from "@angular/platform-browser-dynamic/testing";
 
 declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting(),
+    {teardown: {destroyAfterEach: false}}
 );
 
 // // Then we find all the tests.
